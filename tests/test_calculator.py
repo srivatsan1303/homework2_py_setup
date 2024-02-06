807caf6a32af07bbf5dc43f48a904cabac16babe
srivatsan1303 <sj796@njit.edu>
2024-02-06 06:54:39 +0530

    Updated Part 2

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,10 @@
-'''My Calculator Test'''
-from calculator import add, subtract
+'''Testing the calculator class'''
+from calculator import calculator
 
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
-
-def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+def test_calculator():
+    '''Testing the calculator methods'''
+    assert calculator.add(2,2) == 4
+    assert calculator.subtract(2,2) == 0
+    assert calculator.multiply(2,3) == 6
+    assert calculator.divide(2,2) == 1
     
